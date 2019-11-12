4f07056b835d62bb78c05a658a4c66dc1dafbb15
Christian Stussak <stussak@mfo.de>
2019-11-12 15:07:20 +0100

    Make size and stroke weight configurable

--- spiroscillate/sketch.js
+++ spiroscillate/sketch.js
@@ -1,20 +1,24 @@
 // sin(), cos(), spiral, oscillate, sunflower, starfish
 
+const size = 1080;
+const strokeWidthFactor = 3.0 / 1080.0; // equals a stroke width of 6 on 1080x1080
+
 const num = 200;
 let ang, incr, rad, mult,
     radSpiral, xIn, yIn, x, y;
 let c1, c2;
 
 function setup() {
-    createCanvas(550, 550);
+    createCanvas(size, size);
+    strokeWeight(strokeWidthFactor * size);
     rad = (width / 2) - 10;
     incr = 4.15741;
-    c1 = color(0);
-    c2 = color(225, 0);
+    c1 = color(255, 255);
+    c2 = color(20, 0);
 }
 
 function draw() {
-    background(225);
+    background(0);
 
     xIn = width / 2;
     yIn = height / 2;
@@ -27,7 +31,6 @@ function draw() {
         y = height / 2 + sin(ang) * radSpiral;
 
         stroke(lerpColor(c2, c1, mult));
-        strokeWeight(.5);
         line(xIn, yIn, x, y);
 
         xIn = x;
